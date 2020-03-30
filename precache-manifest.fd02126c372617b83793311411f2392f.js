529ef102bd809f837ce3e5faeacc3f5c916a18c3
Mason <kevmace18@gmail.com>
2020-03-30 22:56:47 +0200

    Updates

---
+++ precache-manifest.fd02126c372617b83793311411f2392f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b82a92894f730769c16ad40875bff9ca",
+    "url": "/freedom/index.html"
+  },
+  {
+    "revision": "69c6b174966262330783",
+    "url": "/freedom/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1f7ad72306fd503fa61b",
+    "url": "/freedom/static/js/2.abf80d5d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/freedom/static/js/2.abf80d5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69c6b174966262330783",
+    "url": "/freedom/static/js/main.a142b238.chunk.js"
+  },
+  {
+    "revision": "f8f3094d99a4895087e4",
+    "url": "/freedom/static/js/runtime-main.0be046ec.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/freedom/static/media/logo.5d5d9eef.svg"
+  }
+]);
